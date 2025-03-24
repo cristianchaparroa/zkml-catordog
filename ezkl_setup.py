cb0e097877dcfb11cf1903fe1652ad5343bdaf75
Cristian Chaparro <cristianchaparroa@gmail.com>
2025-03-24 00:02:17 -0500

    modify params for opt

--- ezkl_setup.py
+++ ezkl_setup.py
@@ -1,6 +1,10 @@
 import ezkl
 import onnx
 
-ezkl.gen_settings('my_model.onnx')
+run_args = ezkl.PyRunArgs()
+run_args.logrows = 10 # This will use 2^10 rows
+run_args.num_inner_cols = 1 # Default is 2, reducing can save memory
+
+ezkl.gen_settings('my_model.onnx', 'settings.json', run_args)
 ezkl.compile_circuit("my_model.onnx", "my_circuit.ezkl", "settings.json")
 ezkl.setup('my_circuit.ezkl', 'vk.key', 'pk.key', 'kzg17.srs')
