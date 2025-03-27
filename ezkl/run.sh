3c5636dde90d7314a50139a401f0a6d2b06c99e9
Cristian <cristianchaparroa@users.noreply.github.com>
2025-03-26 19:18:39 -0500

    modify the ezkl setup artefact script (#20)

--- ezkl/run.sh
+++ ezkl/run.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 # Define variables with original paths
-MODEL_PATH=../classifier/tiny_model.onnx
-CIRCUIT_PATH=my_circuit.ezkl
-VK_PATH=vk.key
-PK_PATH=pk.key
-SRS_PATH=srs17.srs
-SETTINGS_PATH=settings.json
-WITNESS_PATH=witness.json
-PROOF_PATH=proof.json
-INPUT_PATH=input.json
+# setup stage
+MODEL_PATH=classifier/tiny_model.onnx
+CIRCUIT_PATH=backend/artefacts/my_circuit.ezkl
+VK_PATH=backend/artefacts/vk.key
+PK_PATH=backend/artefacts/pk.key
+SRS_PATH=backend/artefacts/srs17.srs
+SETTINGS_PATH=backend/artefacts/settings.json
+
 
 # setup (developer)
 ezkl gen-srs --srs-path $SRS_PATH --logrows 17
-ezkl gen-settings -M $MODEL_PATH -K 17 -N 1 --scale-rebase-multiplier 2
+ezkl gen-settings -M $MODEL_PATH -K 17 -N 1 --scale-rebase-multiplier 1 --settings-path $SETTINGS_PATH
 ezkl compile-circuit -M $MODEL_PATH -S $SETTINGS_PATH --compiled-circuit $CIRCUIT_PATH
 ezkl setup -M $CIRCUIT_PATH --srs-path $SRS_PATH --vk-path $VK_PATH --pk-path $PK_PATH
 
+
+# WITNESS_PATH=backend/artefacts/witness.json
+# PROOF_PATH=backend/artefacts/proof.json
+# INPUT_PATH=backend/artefacts/input.json
 # prove (user interaction)
 # input simulates the user input and we will generate a proof around it
 # ezkl gen-random-data -M $MODEL_PATH --data $INPUT_PATH
